d0674895b6413104c4da4d5cd9b628c6945171f6
JacobPeacey <10693315@student.bpc.ac.uk>
2024-08-19 20:23:36 +0100

    Initial Commit

---
+++ ParkMapper/ParkMapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkMapperApp.swift
+//  ParkMapper
+//
+//  Created by Jacob Peacey on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ParkMapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
