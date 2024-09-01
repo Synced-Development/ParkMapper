c1e710727813dcbda69704f87d59c9fabad1ecf1
JacobPeacey <10693315@student.bpc.ac.uk>
2024-09-01 12:32:13 +0100

    Update AppSettings.swift
    Adds view to create CS case

--- ParkMapper/AppSettings.swift
+++ ParkMapper/AppSettings.swift
@@ -15,6 +15,18 @@ struct AppSettings: View {
                 Spacer()
             }
         } //Page Title
+        Form {
+            Section("Help & Support") {
+                HStack {
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(systemName: "envolope")
+                        Text ("Send an Email to Support")
+                    })
+                }
+            }
+        }
         Spacer()
         HStack {
             Text ("Current app version: \(getAppVersion())")
@@ -22,9 +34,20 @@ struct AppSettings: View {
         } //Current app version
     }
 }
+
 func getAppVersion() -> String {
     if let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
         return appVersion
     }
     return "Unknown"
 } //Gets current application version from dictionary
+
+struct NewSupportCaseView: View {
+    var body: some View {
+        VStack {
+            Text ("Contact Support")
+                .font(.title)
+        } //Page Header
+        Spacer()
+    }
+}
